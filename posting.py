448a31d6a49f98a675198e5d8efc6415eb4fd4b3
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-06 15:38:30 +0800

    Update posting.py

--- posting.py
+++ posting.py
@@ -1,21 +1,18 @@
 from pyrogram import Client, errors
 import asyncio
-import os
 
-# Fetch API credentials from environment variables
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
-session_string = os.getenv("SESSION_STRING")
+# Hardcoded credentials and session string
+api_id = 20240995
+api_hash = "a6cb8eb76c7e91215e483416a29dc0e0"
+session_string = "BQE02mMAEQ83KHZ0hOUyHrS6nCpZSurJ6-xonZklz3Naht9WNBUEwbLSYMZ90aRd6xT0t9gpg5EvGyI2bGx0WiE23S4GREm_jwRKntRj_pM3megHDtyp9gAux_il7GyFZ5dR5lgG-1Z5xVVaGhX2wl3808Q8J2MMFumu8frRmglqDgW8jQ7HPiSzYZb2BvY3nFyEJuXUAEY70J084iQGGYUp1Mr6FyZWy-hQxNc6U8fny7URmfAQxwypyoUQpZqJ62ADgY3fIMRX-Z40AivqYXLw2X-wcml8Lih1ziDTiM0bjTo_p3mQuFKKM5jmB7btei5qHio3tVNSFys7C_C7j5P7GnBcsQAAAAAfcSmPAA"
 
-# Message content with clickable link
+# Message content and list of groups with Chat IDs
 message_content = "https://t.me/mpgoviralgrowthtools (admin share 3 kali MP goviral setiap hari)"
-
-# List of groups with Chat IDs
-groups = {
-    "Group 1": -1001513360832,
-    "Group 2": -1001656152814,
-    # Add more groups as needed
-}
+groups = [
+    -1001513360832, -1001656152814, -1002032128619, -1002065798504,
+    -1002083219543, -1001473865431, -1001194015232, -1002080497857,
+    -1001641889050, -1001338686972, -1002080593272, -1001778610644
+]
 
 # Function to send a message to a target chat
 async def send_message(app, target_chat_id):
@@ -31,7 +28,7 @@ async def send_message(app, target_chat_id):
 # Main async function to setup client and start posting
 async def main():
     async with Client(session_string, api_id=api_id, api_hash=api_hash) as app:
-        tasks = [send_message(app, group_id) for group_id in groups.values()]
+        tasks = [send_message(app, group_id) for group_id in groups]
         await asyncio.gather(*tasks)
 
 # Run the asynchronous main function
